4e7f139799d2f54b28a47ea53be992de04df0c6c
Čamaj Vladimír <vladimir.camaj@gmail.com>
2021-08-22 11:28:07 +0200

    jenkins-deploy.sh

---
+++ jenkins-deploy.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+www_dir=/var/www
+www_new_app_dir=$www_dir/deploy-new-vue.tatrytec.eu
+www_old_app_dir=$www_dir/deploy-old-vue.tatrytec.eu
+
+
+# git clone is in the pipeline gonfiguration
+#git clone https://github.com/camohub/tatrytec-vue-admin.git $www_new_app_dir
+cd $www_new_app_dir
+echo "---------------------------------------------------"
+echo " git clone done "
+echo "---------------------------------------------------"
+
+
+echo "---------------------------------------------------"
+echo " .env file + session files + storage/app copy done "
+echo "---------------------------------------------------"
+
+chmod -R 770 $www_new_app_dir/
+echo "---------------------------------------------------"
+echo " chmod -R 770 done "
+echo "---------------------------------------------------"
+
+npm install
+npm run build
+echo "---------------------------------------------------"
+echo " npm build done "
+echo "---------------------------------------------------"
+
+find $www_new_app_dir -type d -exec chmod 775 {} \;  # chmod for directories
+echo "---------------------------------------------------"
+echo " chmod f + chmod d dome "
+echo "---------------------------------------------------"
+
+mv $www_dir/vue.tatrytec.eu $www_old_app_dir
+echo "---------------------------------------------------"
+echo " old app folder rename done "
+echo "---------------------------------------------------"
+
+mv $www_new_app_dir $www_dir/vue.tatrytec.eu
+echo "---------------------------------------------------"
+echo " new app folder rename done "
+echo "---------------------------------------------------"
+
+cd $www_dir/vue.tatrytec.eu
+
+rm -rf $www_old_app_dir
+echo "---------------------------------------------------"
+echo " DEPLOY IS DONE. CHECK ERROR MESSAGES. "
+echo "---------------------------------------------------"
+
